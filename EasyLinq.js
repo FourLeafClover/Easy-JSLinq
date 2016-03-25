ff2d29a81dbf1987f0eba0e7679b4c10469c3c0c
FourLeafClover <zc_smile@outlook.com>
2016-03-25 22:35:37 +0800

    更改结构. 支持实例化Linq对象.

--- EasyLinq.js
+++ EasyLinq.js
@@ -1,26 +1,46 @@
 (function () {
 
-    var ERRORCode_1 = "Array can not be null";
-    var ERRORCode_2 = "The type of field must be number";
+    var ERRORCode_1 = "Array can not be null or undefined";
+    var ERRORCode_2 = "Please input one param";
     var ERRORCode_3 = "The type of Param is not array";
+    var ERRORCode_4 = "The function only support one param";
 
-    var dataItems = [];
-
-    var Linq = function () {
+    var Creator = function () {
     };
 
+    var Linq = function (dataSource) {
+        this.dataSource = dataSource;
+    };
+
+    Creator.prototype.From = function () {
+        var args = arguments;
+        if (args.length === 0) {
+            throw ERRORCode_2;
+        }
+        if (args.length > 1) {
+            throw ERRORCode_4;
+        }
+        if (args[0] === undefined || args[0] === null) {
+            throw ERRORCode_1;
+        }
+        if (!Array.isArray(args[0])) {
+            throw ERRORCode_3;
+        }
+        return new Linq(args[0]);
+    }
+
     Linq.prototype.All = function (func) {
-        if (dataItems === null) {
+        if (this.dataSource === null) {
             throw ERRORCode_1;
         }
 
-        if (dataItems.length === 0) {
+        if (this.dataSource.length === 0) {
             return false;
         }
 
         var result = true;
-        for (var index = 0; index < dataItems.length; index++) {
-            if (!func(dataItems[index])) {
+        for (var index = 0; index < this.dataSource.length; index++) {
+            if (!func(this.dataSource[index])) {
                 result = false;
                 break;
             }
@@ -29,26 +49,14 @@
     }
 
     Linq.prototype.Add = function (item) {
-
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-        if (item != null) {
-            dataItems.push(item);
-        }
-
+        this.dataSource.push(item);
         return this;
     }
 
     Linq.prototype.Any = function (func) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
         var result = false;
-
-        for (var index = 0; index < dataItems.length; index++) {
-            if (func(dataItems[index])) {
+        for (var index = 0; index < this.dataSource.length; index++) {
+            if (func(this.dataSource[index])) {
                 result = true;
                 break;
             }
@@ -57,49 +65,27 @@
     }
 
     Linq.prototype.Clear = function () {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-        dataItems.splice(0, dataItems.length);
+        this.dataSource.splice(0, this.dataSource.length);
     }
 
     Linq.prototype.Count = function () {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-        return dataItems === null ? 0 : dataItems.length;
-    }
-
-    Linq.prototype.From = function (array) {
-        dataItems = array;
-        return this;
+        return this.dataSource.length;
     }
 
     Linq.prototype.FindIndexOf = function (func) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
         var result = -1;
-
-        for (var index = 0; index < dataItems.length; index++) {
-            if (func(dataItems[index])) {
+        for (var index = 0; index < this.dataSource.length; index++) {
+            if (func(this.dataSource[index])) {
                 result = index;
             }
         }
-
         return result;
     }
 
     Linq.prototype.FindLastIndexOf = function (func) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
         var result = -1;
-
-        for (var index = dataItems.length - 1; index >= 0; index--) {
-            if (func(dataItems[index])) {
+        for (var index = this.dataSource.length - 1; index >= 0; index--) {
+            if (func(this.dataSource[index])) {
                 result = index;
             }
         }
@@ -108,137 +94,91 @@
     }
 
     Linq.prototype.FirstOrDefault = function (func) {
-
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
-        if (dataItems.length === 0) {
+        if (this.dataSource.length === 0) {
             return null;
         }
-
         if (func === undefined) {
-            return dataItems[0];
+            return this.dataSource[0];
         } else {
-            for (var index = 0; index < dataItems.length; index++) {
-                if (func(dataItems[index])) {
-                    return dataItems[index];
+            for (var index = 0; index < this.dataSource.length; index++) {
+                if (func(this.dataSource[index])) {
+                    return this.dataSource[index];
                 }
             }
         }
-
         return null;
     }
 
     Linq.prototype.Foreach = function (func) {
-
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
-        dataItems.forEach(function (item) {
+        this.dataSource.forEach(function (item) {
             func(item);
         });
-
         return this;
     }
 
     Linq.prototype.IndexOf = function (item) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
-        if (item === null) {
-            return -1;
-        }
-        else {
-            return dataItems.indexOf(item);
-        }
-
+        return this.dataSource.indexOf(item);
         return this;
     }
 
     Linq.prototype.Join = function (splitChar) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
-        return dataItems.join(splitChar);
+        return this.dataSource.join(splitChar);
     }
 
     Linq.prototype.LastOrDeafult = function (func) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
-        if (dataItems.length === 0) {
+        if (this.dataSource.length === 0) {
             return null;
         }
-        return dataItems[dataItems.length - 1];
+        return this.dataSource[this.dataSource.length - 1];
     }
 
     Linq.prototype.Pop = function (item) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
-        var index = dataItems.indexOf(item);
+        var index = this.dataSource.indexOf(item);
         if (index > -1) {
-            dataItems.splice(index, 1);
+            this.dataSource.splice(index, 1);
         }
         return this;
     }
 
     Linq.prototype.PopRange = function (items) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
         if (!Array.isArray(items)) {
             throw ERRORCode_3;
         }
-
+        var outer = this;
         items.forEach(function (item) {
-            var index = dataItems.indexOf(item);
+            var index = outer.dataSource.indexOf(item);
             if (index > -1) {
-                dataItems.splice(index, 1);
+                outer.dataSource.splice(index, 1);
             }
         })
         return this;
     }
 
     Linq.prototype.RemoveAll = function (func) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
         var removeItem = [];
-        dataItems.forEach(function (item) {
+        this.dataSource.forEach(function (item) {
             if (func(item)) {
                 removeItem.push(item);
             }
         })
 
+        var outer = this;
         removeItem.forEach(function (item) {
-            var index = dataItems.indexOf(item);
-            dataItems.splice(index, 1);
+            var index = outer.dataSource.indexOf(item);
+            outer.dataSource.splice(index, 1);
         });
 
         return this;
     }
 
     Linq.prototype.Sum = function (func) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
-        if (dataItems.length === 0) {
+        if (this.dataSource.length === 0) {
             return 0;
         }
         var sum = 0;
 
         if (func === undefined) {
-            dataItems.forEach(function (item) {
+            this.dataSource.forEach(function (item) {
                 if (typeof(item) === 'number') {
                     sum += item;
                 } else {
@@ -247,7 +187,7 @@
             });
         }
         else {
-            dataItems.forEach(function (item) {
+            this.dataSource.forEach(function (item) {
                 var iterator = func(item);
                 if (typeof(iterator) === 'number') {
                     sum += iterator;
@@ -260,33 +200,22 @@
     }
 
     Linq.prototype.Select = function (func) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
         var result = [];
 
-        dataItems.forEach(function (item) {
+        this.dataSource.forEach(function (item) {
             result.push(func(item));
         })
 
-        dataItems = result;
+        this.dataSource = result;
         return this;
     }
 
     Linq.prototype.Sort = function (func) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-        dataItems.sort(func);
+        this.dataSource.sort(func);
         return this;
     }
 
     Linq.prototype.AddRange = function (array) {
-        if (dataItems === null) {
-            throw ERRORCode_1;
-        }
-
         if (array === null) {
             return this;
         }
@@ -295,34 +224,31 @@
             throw ERRORCode_3;
         }
 
+        var outer = this;
         array.forEach(function (item) {
-            dataItems.push(item);
+            outer.dataSource.push(item);
         });
 
         return this;
     }
 
     Linq.prototype.ToArray = function () {
-        return dataItems;
+        return this.dataSource;
     }
 
     Linq.prototype.Where = function (func) {
-        if (dataItems === null) {
-            throw  ERRORCode_1;
-        }
         var result = [];
-
-        dataItems.forEach(function (item) {
+        this.dataSource.forEach(function (item) {
             if (func(item)) {
                 result.push(item);
             }
         });
-        dataItems = result;
+        this.dataSource = result;
         return this;
     }
 
     if (window.EasyLinq === undefined || window.EasyLinq === null) {
-        window.EasyLinq = new Linq();
+        window.EasyLinq = new Creator();
     }
 
 })(window.EasyLinq);
--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # Easy-JSLinq
 Linq For JS
 
-基于JS 的Linq 扩展.
+基于JS 的Linq 扩展.因为考虑如果直接在原型修改会破坏原型。尤其是在公司里面很多Team在一个大的平台开发。如果所有Team都在原型扩展方法，
+导致的后果是。如果不同Team里面原型扩展名一样可是功能不一样就会比较麻烦。所以就只能单独写一个Linq库.来处理。
 
-主要是将.net的Linq移植到JS上.
-还在进行中. 在4月初前完成. 包括单元测试覆盖完毕.
+主要是将.net的Linq移植到JS上.对.Neter友好.对Java可能不太友好
+
+1.支持链式编程.
+2.调用From方法会实例化一个Object对象.dataSource是传进去的Array对象
+{
+   dataSource:Array
+   .property
+}
+3. 类似于.net的Linq.
+  3.1 Where,FirstOrdefault... 取出来的都是From传进去的source里面的元素
+  3.2 Add,Remove 也是会改变From 传进去的source数组的
+  3.3 调用Select后会重新去实例化对象里面的dataSource. 但是不会去改变From方法传进去的source
+
+var linq1 = EasyLinq.From([1,2,4]);
+var sum = linq1.Sum(); // 7
+var array2 = EasyLinq.From([1,2,4]).Select(function(item){
+   return item*2;
+}).ToArray(); // [2,4,8]
+
+var array2 = EasyLinq.From([1,2,4]).Select(function(item){
+       return item*2;
+    }).Sum(); // 14
+
+
